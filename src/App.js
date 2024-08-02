83a9c5c85cf66bd0347e3a474e979f92a2aa8258
husham <hishamalmged@gmail.com>
2024-08-02 03:18:32 -0700

    second branch

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 function App() {
   return (
     <section>
-      <h1 className="title">new branch</h1>
+      <h1 className="title">new branch 2</h1>
       <h1 className="title">hello react </h1>
       <h1>husham jad </h1>
     </section>
