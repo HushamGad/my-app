1c0c11ca90317d4d409bc5d821c441d329bb5544
husham <hishamalmged@gmail.com>
2024-08-04 02:06:16 -0700

    all pages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Home from './pages/home';
+import Html from './pages/html';
+import Css from './pages/css';
+import Javascript from './pages/javascript';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+    errorElement: <h1>OPS There Is Error</h1>
+  },
+  {
+    path: "/html",
+    element: <Html/>,
+  },
+  {
+    path: "/css",
+    element: <Css/>,
+  },
+  {
+    path: "/javascript",
+    element: <Javascript/>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ src/pages/home.js
@@ -0,0 +1,143 @@
+
+const Home = () => {
+    return (
+              <>
+  <header className="hide-when-mobile">
+    <h1>Css First Project **</h1>
+    <ul className="flex">
+      <li className="main-list">
+        <a className="main-link" href="#">
+          HTML
+        </a>
+        <ul className="sub-ul">
+          <li>
+            <a href="#">Full Course</a>
+          </li>
+          <li>
+            <a href="#">Crash Course</a>
+          </li>
+          <li>
+            <a href="#">Learn in 1n</a>
+          </li>
+        </ul>
+      </li>
+      <li className="main-list">
+        <a href="#" className="main-link">
+          CSS
+        </a>
+        <ul className="sub-ul">
+          <li>
+            <a href="#">Full Course</a>
+          </li>
+          <li>
+            <a href="#">Css Example</a>
+          </li>
+          <li className="mini-projects">
+            <a href="#">mini projects&nbsp;+</a>
+            <ul className="sub-sub-ul">
+              <li>
+                <a href="#">Project 1</a>
+              </li>
+              <li>
+                <a href="#">Project 2</a>
+              </li>
+              <li>
+                <a href="#">Project 3</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </li>
+      <li className="main-list">
+        <a href="#" className="main-link">
+          JAVASCRIPT
+        </a>
+        <ul className="sub-ul sub-of-js">
+          <li>
+            <a href="#">Coming soon🔥</a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </header>
+  <header className="show-when-mobile">
+    <div className="navbar">
+      <h1>Css First Project</h1>
+      <label htmlFor="menu">
+        <i className="fa-solid fa-bars" />
+      </label>
+    </div>
+    <input type="checkbox" id="menu" />
+    <div className="show-on-click">
+      <div className="main-div">
+        <label htmlFor="html">
+          HTML <i className="fa-solid fa-plus" />
+        </label>
+        <input type="checkbox" id="html" />
+        <ul className="sub-div">
+          <li>
+            <a href="#">Full Course</a>
+          </li>
+          <li>
+            <a href="#">Crash Course</a>
+          </li>
+          <li>
+            <a href="#">Learn in 1n</a>
+          </li>
+        </ul>
+      </div>
+      <div className="main-div">
+        <label htmlFor="css">
+          CSS <i className="fa-solid fa-plus" />
+        </label>
+        <input type="checkbox" id="css" />
+        <ul className="sub-div">
+          <li>
+            <a href="#">Full Course</a>
+          </li>
+          <li>
+            <a href="#">Css Example</a>
+          </li>
+          <label htmlFor="mini" className="mini-projects">
+            mini projects <i className="fa-solid fa-plus" />
+          </label>
+          <input type="checkbox" id="mini" />
+          <ul className="sub-sub-div">
+            <li>
+              <a href="#">Project 1</a>
+            </li>
+            <li>
+              <a href="#">Project 2</a>
+            </li>
+            <li>
+              <a href="#">Project 3</a>
+            </li>
+          </ul>
+        </ul>
+      </div>
+      <div className="main-div">
+        <label htmlFor="js">
+          JAVASCRIPT
+          <i className="fa-solid fa-plus" />
+        </label>
+        <input type="checkbox" id="js" />
+        <ul className="sub-div sub-of-js">
+          <li>
+            <a href="#">Coming soon🔥</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </header>
+  <main>
+    Home Page
+  </main>
+  <footer>
+    DDDesigned and developed by Husham jad<span>🧡</span>
+  </footer>
+</>
+
+    );
+}
+
+export default Home;
